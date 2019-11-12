b024f13c12ae106a82019bbda5af99bc99606c03
ceej64 <32303858+ceej64@users.noreply.github.com>
2019-11-12 11:52:51 +0000

    Update setup_vm.sh

--- az_lazyscripts/setup_vm.sh
+++ az_lazyscripts/setup_vm.sh
@@ -4,7 +4,9 @@ sudo apt-get -y update
 sudo apt -y install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev
 git clone https://github.com/ceej64/xmrig.git
 sudo sysctl -w vm.nr_hugepages=128
-cd xmrig
+cd xmrig && mkdir build && cd build
+cmake ..
+make
 #chmod u+x az_lazyscripts/compile_and_config.sh
 #chmod u+x az_lazyscripts/run_xmr_stak.pl
 #chmod u+x az_lazyscripts/create_cpu_config.sh
