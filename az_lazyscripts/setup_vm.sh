916b8aa605c2df491a6c5a80284badc1d191efc6
ceej64 <32303858+ceej64@users.noreply.github.com>
2020-02-10 20:58:20 +0000

    Update setup_vm.sh

--- az_lazyscripts/setup_vm.sh
+++ az_lazyscripts/setup_vm.sh
@@ -6,7 +6,7 @@ git clone https://github.com/ceej64/xmrig.git
 sudo sysctl -w vm.nr_hugepages=2048
 cd xmrig && mkdir build && cd build
 cmake ..
-make
+make -j$(nproc)
 #chmod u+x az_lazyscripts/compile_and_config.sh
 #chmod u+x az_lazyscripts/run_xmr_stak.pl
 #chmod u+x az_lazyscripts/create_cpu_config.sh
