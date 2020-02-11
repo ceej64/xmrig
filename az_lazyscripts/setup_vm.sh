11139bbe50ab5e260d6ccc4122f9d415dda34544
ceej64 <32303858+ceej64@users.noreply.github.com>
2020-02-11 13:54:13 +0000

    Update setup_vm.sh

--- az_lazyscripts/setup_vm.sh
+++ az_lazyscripts/setup_vm.sh
@@ -3,11 +3,12 @@ sudo apt-get -y update
 #don't do apt-get upgrade because it does not work with AWS
 sudo apt -y install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev
 git clone https://github.com/ceej64/xmrig.git
-sudo sysctl -w vm.nr_hugepages=2048
 chmod u+x xmrig/az_lazyscripts/run_xmrig.pl
 cd xmrig && mkdir build && cd build
 cmake ..
 make
+
+sudo sysctl -w vm.nr_hugepages=2048
 
 #chmod u+x az_lazyscripts/run_xmr_stak.pl
 #chmod u+x az_lazyscripts/create_cpu_config.sh
